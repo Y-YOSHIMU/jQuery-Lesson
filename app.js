4d4eccc9ff1dd9a025496870be0bc7fcf7c71d38
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-06-25 17:40:49 +0900

    [Finish]6章

--- app.js
+++ app.js
@@ -24,12 +24,33 @@
 // });
 
 // 5章確認問題
+// $(function(){
+// 	$('.box1').slideDown(1000,function(){
+// 		$('.box1').css({
+// 			'background-color': '#0000FF',
+// 			'width': '200px',
+// 			'height': '100px'
+// 		}).slideUp();
+// 	});
+// });
+
+// 6章開始
+// $(function(){
+// 	$('.box1').mouseover(function(){
+// 		$('.box1').addClass('box1-ext');
+// 	});
+
+// 	$('.box1').mouseout(function(){
+// 		$('.box1').removeClass('box1-ext');
+// 	});
+// });
+
 $(function(){
-	$('.box1').slideDown(1000,function(){
-		$('.box1').css({
-			'background-color': '#0000FF',
-			'width': '200px',
-			'height': '100px'
-		}).slideUp();
+	$('.box1').on('click', function(){
+		$('.box1').addClass('box1-ext');
+	});
+
+	$('.box1').mouseout(function(){
+		$('.box1').removeClass('box1-ext');
 	});
 });
