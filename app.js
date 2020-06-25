d61d5562ee2bb922337166031a2cfea4f34a511e
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-06-25 16:23:21 +0900

    [Finish]5章

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-$(document).ready(function () {
-	$('body').html('<h1>Hello jQuery!!</h1>');
+// $(document).ready(function () {
+// 	$('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// ここから5章
+// $(function(){
+// 	$('.box1').css({
+// 		'background-color': '#0000ff',
+// 		'height': '100px'
+// 	});
+// });
+
+// $(function(){
+// 	$('.box1').slideUp();
+// });
+
+// $(function(){
+// 	$('.box1').show();
+// 	$('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+// 	$('.box1').hide();
+// });
+
+// 5章確認問題
+$(function(){
+	$('.box1').slideDown(1000,function(){
+		$('.box1').css({
+			'background-color': '#0000FF',
+			'width': '200px',
+			'height': '100px'
+		}).slideUp();
+	});
 });
