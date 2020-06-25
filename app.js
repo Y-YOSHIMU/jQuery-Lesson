c3fecfbdc82ed9e546e4ef6e57c43a01dc312dc4
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-06-25 20:08:18 +0900

    [Finish]7章

--- app.js
+++ app.js
@@ -45,12 +45,42 @@
 // 	});
 // });
 
-$(function(){
-	$('.box1').on('click', function(){
-		$('.box1').addClass('box1-ext');
-	});
+// $(function(){
+// 	$('.box1').on('click', function(){
+// 		$('.box1').addClass('box1-ext');
+// 	});
 
-	$('.box1').mouseout(function(){
-		$('.box1').removeClass('box1-ext');
+// 	$('.box1').mouseout(function(){
+// 		$('.box1').removeClass('box1-ext');
+// 	});
+// });
+
+// 7章開始
+// $(function(){
+// 	$('.bg1').on('click', function(){
+// 		$('.bg1').slideUp();
+// 	});
+// 	$('.bg2').on('click', function(){
+// 		$('.bg2').slideUp();
+// 	});
+// 	$('.bg3').on('click', function(){
+// 		$('.bg3').slideUp();
+// 	});
+// 	$('.bg4').on('click', function(){
+// 		$('.bg4').slideUp();
+// 	});
+// });
+
+// thisで書き換える
+// $(function(){
+// 	$('.box1').on('click', function(){
+// 		$(this).slideUp();
+// 	});
+// });
+
+// childrenを使う
+$(function(){
+	$('button').on('click', function(){
+		$('ul').children().css('color', 'red');
 	});
 });
